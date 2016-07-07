13dea8805e6f7063aeed3b64886bfbc8188d5ef5
nocs00 <nocs00@gmail.com>
2016-07-08 00:01:48 +0300

    added support of Spring REST controller

---
+++ src/main/java/com/ufood/controller/RestController.java
@@ -0,0 +1,12 @@
+package com.ufood.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by pdudenkov on 03.07.2016.
+ */
+
+@Controller
+public class RestController {
+
+}
